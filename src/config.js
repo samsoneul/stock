0a9a3b085c40e0561295f975f859be756ef79d7e
samsoneul <samsonleul2@gmail.com>
2024-01-29 14:40:53 +0300

    first

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const chartconfig={
+    "1D":{day :1,weeks:0,months:0,years:0, resolution:"1"},
+    "1W":{day :0,weeks:1,months:0,years:0,resolution:"15"},
+    "1M":{day :0,weeks:0,months:1,years:0,resolution:"60"},
+    "1Y":{day :0,weeks:0,months:0,years:1,resolution:"D"},
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: [
+        ["Quicksand","sans-serif"],
+      ],
+    },
+    gridTemplateRows:{
+      7:"repeat(7,minmax(0,1fr))",
+      8:"repeat(8,minmax(0,1fr))",
+
+    },
+  },
+  plugins: [],
+}
+
